f7b3e7e0a4e7e725f30411a25215920f9b2d888e
Reed McDaniel <reed.mcdaniel@cityblock.com>
2024-09-12 18:06:36 -0400

    more routing

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from './services/Menu.js';
+import Router from './services/Router.js';
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 // want to know when the DOM is finished being constructed (ready for manipulation)
 // happens after script's defer i.e. this js file is downloaded and parsed
 // hapens before render
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
 window.addEventListener("DOMContentLoaded", async () => {
+  app.router.init();
   loadData();
-  
 });
---
+++ services/Router.js
@@ -0,0 +1,69 @@
+const Router = {
+  init: () => {
+    // for each nav link, change default click behavior
+    document.querySelectorAll('a.navlink').forEach((a) => {
+      a.addEventListener('click', (event) => {
+        event.preventDefault();
+        // handle routing by going to the href of the clicked link
+        const url = event.target.getAttribute('href');
+        Router.go(url);
+      })
+    })
+
+    // event handler for URL changes i.e. popstate event emitted when pressing back or forward button
+    window.addEventListener("popstate", (event) => {
+      // accessible from the the data arg passed to history.pushState
+      // addToHistory = false becasue we are navigating the existing histor with <- and ->
+      Router.go(event.state.route, false)
+    })
+
+    // Check the initial URL and start there
+    // you may have copy and pasted the link from elsewhere and it need not be the homepage
+    Router.go(location.pathname);
+
+    // to handle a refresh, must configure server to respond appropriately (out of scope)
+
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+
+    if(addToHistory){
+      history.pushState({ route }, null, route);
+    }
+
+    // choosing the option of injecting and removing DOM elements
+    // rather than hiding elements
+    let pageElement = null;
+    switch(route) {
+      case '/':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Menu';
+        break;
+      case '/order':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Your Order';
+        break;
+      default:
+        if (route.startsWith('/product-')) {
+          pageElement = document.createElement('h1');
+          pageElement.textContent = 'Details'; 
+          const paramId = route.substring(route.lastIndexOf('-') + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    if (pageElement){
+      const cache = document.querySelector('main');
+      // clear everything in the main element
+      // alternative: document.querySelector('main').children[0].remove();
+      cache.innerHTML = '';
+    
+      cache.appendChild(pageElement);
+
+      // scroll back to top
+      window.scroll(0,0)
+    }
+  }
+}
+
+export default Router;
